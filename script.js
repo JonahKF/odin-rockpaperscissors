dbfd88ea99cf6d9ba79362404bcf7d3e571b85a4
Jonah <jonahkf@gmail.com>
2024-09-04 18:48:55 +0900

    COMPLETE: Odin Rock Paper Scissors Project 1
    Added JS to create RPS game with 5 rounds
    Console counts and displays score each round
    Final check to see who won overall, or if tie
    Used While loop to allow multiple inputs if
    user inputs something besides a valid input.

---
+++ script.js
@@ -0,0 +1,71 @@
+function getComputerChoice() {
+  randInt = Math.random();
+  if (randInt <= 0.33) {
+    return "rock";
+  } else if (randInt >= 0.33 && randInt <= 0.66) {
+    return "paper";
+  } else {
+    return "scissors";
+  }
+}
+
+function getHumanChoice() {
+  while (true) {
+    humanInput = prompt();
+    if (humanInput.toLowerCase() === "rock") {
+      return "rock";
+    } else if (humanInput.toLowerCase() === "paper") {
+      return "paper";
+    } else if (humanInput.toLowerCase() === "scissors") {
+      return "scissors";
+    } else {
+      console.log("Not a valid input!");
+    }
+  }
+}
+
+function playGame() {
+  let humanScore = 0;
+  let computerScore = 0;
+
+  function playRound(humanChoice, computerChoice) {
+    if (
+      (humanChoice === "rock" && computerChoice === "paper") ||
+      (humanChoice === "paper" && computerChoice === "scissors") ||
+      (humanChoice === "scissors" && computerChoice === "rock")
+    ) {
+      console.log("You lose!");
+      computerScore++;
+    } else if (
+      (humanChoice === "rock" && computerChoice === "scissors") ||
+      (humanChoice === "paper" && computerChoice === "rock") ||
+      (humanChoice === "scissors" && computerChoice === "paper")
+    ) {
+      console.log("You win!");
+      humanScore++;
+    } else {
+      console.log("It's a tie!");
+    }
+  }
+
+  for (let i = 0; i < 5; i++) {
+    const humanSelection = getHumanChoice();
+    const computerSelection = getComputerChoice();
+
+    playRound(humanSelection, computerSelection);
+    // console.log(computerSelection);
+    console.log("User: " + humanScore);
+    console.log("CPU: " + computerScore);
+  }
+
+  if (humanScore > computerScore) {
+    console.log("User wins!");
+  } else if (humanScore < computerScore) {
+    console.log("CPU wins!");
+  } else {
+    console.log("It's a tie!");
+  }
+}
+
+// Game Start
+playGame();
